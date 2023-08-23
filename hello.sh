bae46ce60547c81622dc4051daa21abae213e456
akshayjadhavar <142891653+akshayjadhavar@users.noreply.github.com>
2023-08-23 12:43:46 +0530

    Updated hello.sh again

--- hello.sh
+++ hello.sh
@@ -11,3 +11,5 @@ echo "That'll be all. Thank you."
 echo
 echo "This line is added to check the git pull command."
 echo
+echo "Now checking if I can pull this file in local repos without doing a pull request."
+echo
